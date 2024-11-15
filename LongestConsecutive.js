6271a0439fe276cbeb7977d19dfe70a02511487d
pjha7 <prashant_jha1@intuit.com>
2024-11-15 22:52:45 +0530

    LongestConsecutive

---
+++ LongestConsecutive.js
@@ -0,0 +1,16 @@
+const longestConsecutive = (nums) => {
+  const set = new Set(nums);
+  let longest = 0;
+  for (let n of set) {
+    if (!set.has(n - 1)) {
+      let length = 1;
+      while (set.has(n + length)) {
+        length++;
+      }
+      longest = Math.max(longest, length);
+    }
+  }
+  return longest;
+};
+
+console.log(longestConsecutive([2, 20, 4, 10, 3, 4, 5]));
